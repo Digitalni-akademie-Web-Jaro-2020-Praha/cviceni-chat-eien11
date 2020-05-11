76065095d70cabad0a16a76aa72ef33157b482cf
Adela <adela.matouskova1@gmail.com>
2020-05-11 20:42:16 +0200

    Receive messages

--- index.js
+++ index.js
@@ -2,18 +2,42 @@
 
 /* ========== PŘIJÍMÁNÍ ZPRÁV ========== */
 
-const messagesElement = document.querySelector('#messages');
+let messagesElement = document.querySelector('#messages');
 
 const renderMessage = (name, message, date) => {
   // @TODO: funkce vracející HTML zprávy
+  const messageElm = document.createElement('div');
+  messageElm.innerHTML =
+    (`
+        <div class="card mt-3 mb-3">
+					<div class="card-body">
+						<h5 class="card-title">
+							${name}
+							<small class="text-muted">${date}</small>
+						</h5>
+						<p class="card-text">${message}</p>
+					</div>
+				</div>
+  `);
+  return messageElm;
 };
 
 const renderMessages = (messages) => {
   // @TODO: funkce vypisující zprávy na stránku
+  messagesElement.innerHTML = '';
+  for (let i = 0; i < messages.length; i++) {
+    const message = renderMessage(messages[i].name, messages[i].message, messages[i].date);
+    messagesElement.appendChild(message);
+  }
 };
+
 
 const updateMessages = () => {
   // @TODO: funkce stahující zprávy ze server a přidávající je na stránku
+  fetch('https://czechichat.herokuapp.com/api/list-messages')
+    .then(response => response.json())
+    .then(data => renderMessages(data.messages))
+
 };
 
 setInterval(updateMessages, 2000); // Každé dvě sekundy zavolá updateMessages
